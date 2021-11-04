ccf6b742ac10e6ac83b8949bd642c122e9d9e56f
txu6 <txu6@trip.com>
2021-11-04 23:17:34 +0800

    PROGRAMMING NOTES OF EDWINXU

--- auto-push.bat
+++ auto-push.bat
@@ -1,8 +1,5 @@
 git add .
-<<<<<<< HEAD
 git commit -m"PROGRAMMING NOTES OF EDWINXU"
-=======
 git commit -m"Edwin Xu's Programming Notes. Make yourself better!"
->>>>>>> fb6bc71e1e0d44d8b2b4e1d5d043f3890f425311
 git push origin master
 exit
